92fc57c311b63e5c6b8531c9e21f2e03cbaa0267
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-20 15:15:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {};
 playlist.britney = "Oops, I did it again";
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
 }
