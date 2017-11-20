62350abf058f68894572624b18b7c95792203a51
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-20 15:16:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ playlist.britneySpears = "Oops, I did it Again";
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
